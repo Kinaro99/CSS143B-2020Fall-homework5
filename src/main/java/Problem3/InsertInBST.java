4740f9c2b507ece82ff48800019003fb8fd0c3e4
Kinaro99 <abbie687@uw.edu>
2020-11-15 23:47:39 -0800

    prb 3->laptop

--- src/main/java/Problem3/InsertInBST.java
+++ src/main/java/Problem3/InsertInBST.java
@@ -4,6 +4,28 @@ import Problem1.TreeNode;
 
 public class InsertInBST {
     public static void insert(TreeNode<Integer> root, int valToInsert) {
-        // homework
+
+        if(root == null) {
+            return;
+        }
+
+            if(root.val > valToInsert){
+                if(root.left != null) {
+                    insert(root.left, valToInsert);
+                } else {
+                    root.left = new TreeNode<>(valToInsert);
+                }
+            } else {
+                if (root.right != null) {
+                    insert(root.right, valToInsert);
+                } else {
+                    root.right = new TreeNode<>(valToInsert);
+                }
+            }
+
+
     }
 }
+
+//iter -> while/do-while loop
+//recu -> simpler?
